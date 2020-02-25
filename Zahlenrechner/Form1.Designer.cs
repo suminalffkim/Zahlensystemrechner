a097efccffea1fe633d2039892ca98bd6c12c936
Sumin Kim <61191808+suminkimalff@users.noreply.github.com>
2020-02-25 16:19:09 +0100

    first commit

---
+++ Zahlenrechner/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Zahlenrechner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(19, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(143, 114);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Artifakt Element", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton4.Location = new System.Drawing.Point(3, 85);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(92, 20);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Hexadezimal";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Artifakt Element", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(3, 62);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(67, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Dezimal";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Artifakt Element", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(3, 39);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Oktal";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Artifakt Element", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(3, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(53, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Binär";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Location = new System.Drawing.Point(1, 38);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(530, 199);
+            this.panel2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Artifakt Element", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(19, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Rechnen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(168, 27);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(330, 150);
+            this.panel4.TabIndex = 0;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(1, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(530, 40);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Zahlenrechner.Properties.Resources.Unbenannt;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(493, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 242);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Zahlensystem";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Zahlenrechner/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Zahlenrechner
+{
+    public partial class Form1 : Form
+    {
+        int zahl1, dezimalzahl,switchcase;
+        string dual, oktal, dez, hexa;
+        Color backgroundColor = Color.FromArgb(206, 212, 221);
+        Color fontColor= Color.FromArgb(14, 28, 51);
+        Font drawFont = new Font("Artifakt Element", 13);
+
+
+        public Form1()
+        {    
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form1 backform = new Form1();
+
+            panel1.Top = backform.Top;
+            panel1.Width = backform.Width;
+            panel1.Height = 30;
+            panel1.BackColor = Color.FromArgb(218,222,230);
+
+            panel2.Top = panel1.Height;
+            panel2.Height = backform.Height;
+            panel2.Left = backform.Left;
+            panel2.Width = backform.Width;
+            panel2.BackColor = backgroundColor;
+
+            pictureBox1.Left = 506;
+            pictureBox1.Top = 7;
+            pictureBox1.Width = 15;
+            pictureBox1.Height = 15;
+            
+            
+        }
+        //상단바 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = panel1.CreateGraphics();
+            SolidBrush drawbrush = new SolidBrush(Color.FromArgb(14,28,51));
+            canvas.DrawString("Zahlensystem", new Font("Alef", 16), drawbrush, new Point(5, 3));
+        }
+
+        //Table 
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+            
+            Graphics table = panel4.CreateGraphics();
+            Pen blackPen = new Pen(Color.Black);
+            blackPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            for (int i = 1; i <= 5; i++)
+            {
+                table.DrawLine(blackPen, 0, 30 * i, 330, 30 * i);
+            }
+            table.DrawLine(blackPen, 330 / 2, 0, 330 / 2, 150);
+            SolidBrush drawPaint = new SolidBrush(Color.Black);
+            drawPaint.Color = Color.Gray;
+            table.FillRectangle(drawPaint, 0, 0, 330, 30);
+            drawPaint.Color = Color.Black;
+            Font drawFont = new Font("Artifakt Element", 13);
+            table.DrawString("Zahlensystem", drawFont, drawPaint, 6, 6);
+            table.DrawString("Binär", drawFont, drawPaint, 6, 36);
+            table.DrawString("Oktal", drawFont, drawPaint, 6, 66);
+            table.DrawString("Dezimal", drawFont, drawPaint, 6, 96);
+            table.DrawString("Hexadezimal", drawFont, drawPaint, 6, 126);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rechnen();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //dual
+            if (radioButton1.Checked == true)
+                switchcase=1;
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            //oktal
+            if (radioButton2.Checked == true)
+                switchcase = 2;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            //dezimal
+            if (radioButton3.Checked == true)
+                switchcase = 3;
+            
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            //hexadezimal
+            if (radioButton4.Checked == true)
+                switchcase = 4;
+        }
+
+        void rechnen()
+        {
+            //Clear
+            Graphics table = panel4.CreateGraphics();
+            SolidBrush drawPaint = new SolidBrush(Color.FromArgb(206, 212, 221));
+            table.FillRectangle(drawPaint, 170, 125, 150, 25);
+            table.FillRectangle(drawPaint, 170, 35, 150, 25);
+            table.FillRectangle(drawPaint, 170, 65, 150, 25);
+            table.FillRectangle(drawPaint, 170, 95, 150, 25);
+
+            zahl1 = Convert.ToInt32(textBox1.Text);
+
+            switch (switchcase)
+            {
+                //dual
+                case 1:
+                    try
+                    {
+                        dual = zahl1.ToString();
+                        dezimalzahl = Convert.ToInt32(dual, 2);
+                        dez = dezimalzahl.ToString();
+                        oktal = Convert.ToString(dezimalzahl, 8);
+                        hexa = Convert.ToString(dezimalzahl, 16);
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Die gegebene Zahl ist nicht binär", "ERROR", MessageBoxButtons.OK);
+                        panel4.BackColor = backgroundColor;
+                        table.DrawString("Die gegebene Zahl ist nicht binär", new Font("Artifakt Element", 13), drawPaint, 5, 5);
+                    }
+                    break;
+
+                //oktal
+                case 2:
+                    oktal = zahl1.ToString();
+                    dezimalzahl = Convert.ToInt32(oktal, 8);
+                    dez = dezimalzahl.ToString();
+                    dual = Convert.ToString(dezimalzahl, 2);
+                    hexa = Convert.ToString(dezimalzahl, 16);
+                    break;
+
+                //dezimal
+                case 3:
+                    dezimalzahl = zahl1;
+                    dual = Convert.ToString(dezimalzahl, 2);
+                    oktal = Convert.ToString(dezimalzahl, 8);
+                    dez = dezimalzahl.ToString();
+                    hexa = Convert.ToString(dezimalzahl, 16);
+                    break;
+
+                //hexa
+                case 4:
+                    hexa = Convert.ToString(zahl1);
+                    dezimalzahl = Convert.ToInt32(hexa, 16);
+                    dez = dezimalzahl.ToString();
+                    dual = Convert.ToString(dezimalzahl, 2);
+                    oktal = Convert.ToString(dezimalzahl, 8);
+                    break;
+            }
+
+            //paint
+            drawPaint.Color = Color.Black;
+            Font drawFont = new Font("Artifakt Element", 13);
+
+            table.DrawString(dual, drawFont, drawPaint, 175, 36);
+            table.DrawString(oktal, drawFont, drawPaint, 175, 66);
+            table.DrawString(dez, drawFont, drawPaint, 175, 96);
+            table.DrawString(hexa, drawFont, drawPaint, 175, 126);
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            rechnen();
+        }
+                
+    }
+}
